660dbb4a762e56ab2fd0cd467e698fe859813470
Anil Maurya <anil.empi@gmail.com>
2020-07-26 17:47:10 +0530

    game over

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # color-game
-RGB Color Identification Game
+ 
+RGB Color Identification game. It is based on the RGB color system. This game can help to improve RGB Color identification skill.
+ 
+## RGB Color Model
+ 
+RGB color model is an additive color model in which red, green and blue colors are mixed together in various proportions to form a different array of colors.
+ 
+[Read More...](https://en.wikipedia.org/wiki/RGB_color_model)
+ 
+ 
+## How to Run game
+ 
+1. Download or Clone repository
+2. open `index.html` in the browser.
+ 
+Note - Make user `colorgame.js` and `style.css` are in the root directory.
+ 
+ 
+ 
+## Live Demo
+ 
+A lookalike project is hosted on `https://guessthecolor.com/`
+ 
+**Note**
+ 
+Neither I am the owner of the hosted version of the game nor this project's code is copied from there. Only the idea of functionality is taken from there.
+ 
+[Live Demo](https://guessthecolor.com/)
+
+
---
+++ colorgame.js
@@ -0,0 +1,88 @@
+let squareCounts
+let colors = []
+let mode = 'easy'
+const squares = document.querySelectorAll('.square')
+const resetButton = document.querySelector("#reset")
+const easyButton = document.querySelector('#easy')
+const hardButton = document.querySelector('#hard')
+const titleElem = document.querySelector('#title')
+const messageElem = document.querySelector('#message')
+const header = document.querySelector('h1')
+
+init()
+
+function init(){
+    reset()
+}
+
+resetButton.addEventListener('click', reset)
+easyButton.addEventListener('click', toggleMode)
+hardButton.addEventListener('click', toggleMode)
+
+
+function reset(){
+    if(mode === 'easy'){
+        squareCounts = 3
+        easyButton.classList.add('selected')
+        hardButton.classList.remove('selected')
+    }else{
+        squareCounts = 6
+        hardButton.classList.add('selected')
+        easyButton.classList.remove('selected')
+    }
+    colors = []
+    for (let index = 0; index < 6 ; index++) {
+        if(index < squareCounts){
+            const color = getRandomColor()
+            colors.push(color)
+            squares[index].style.backgroundColor = color;
+            squares[index].style.display = 'block'
+            squares[index].addEventListener('click', handleSquareClick)
+        }else{
+            squares[index].style.display = 'none'
+        }
+    }
+
+    titleElem.textContent = pickColor()
+    messageElem.textContent = ''
+    header.style.backgroundColor = 'steelblue'
+}
+
+function pickColor(){
+    const color = Math.floor(Math.random() * colors.length)
+    return colors[color]
+}
+
+function getRandomColor(){
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+function toggleMode(){
+    mode = this.textContent.toLowerCase()
+    reset()
+}
+
+function changeColor(color){
+    for (let index = 0; index < squareCounts; index++) {
+        squares[index].style.background = color
+        squares[index].style.display = 'block'
+    }
+}
+
+function handleSquareClick(){
+    const color = this.style.backgroundColor
+    console.log(color)
+    if( color === titleElem.textContent ){
+        messageElem.textContent = 'You Won !!'
+        resetButton.textContent = 'play again ?'
+        header.style.backgroundColor = color
+        changeColor(color)
+    }else{
+        messageElem.textContent = 'Try Again !!'
+        this.style.display = 'none'
+    }
+}
